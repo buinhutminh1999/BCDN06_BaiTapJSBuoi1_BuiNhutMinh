c0229c8b420a0a8da3e46cbbc4e14e403c4752a9
buinhutminh1999 <buinhutminh1999@gmail.com>
2022-10-09 20:47:05 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /**
  * Bài 1: Viết chương trình tính lương nhân viên
  * Input: oneSalary = 100.000, numDay
- * Proccess: oneSalery * numDay
+ * Process: oneSalery * numDay
  * Output: salary
  */
 
@@ -14,7 +14,7 @@ console.log('Lương nhân viên: ',salary);
 /**
  * Bài 2: Viết chương trình nhập vào 5 số thực tùy ý
  * Input: soThuc1, soThuc2, soThuc3, soThuc4, soThuc5
- * Proccess: (soThuc1 + soThuc2 + soThuc3 + soThuc4 + soThuc5 ) / 5
+ * Process: (soThuc1 + soThuc2 + soThuc3 + soThuc4 + soThuc5 ) / 5
  * Output: giatriTb
  */
 var soThuc1 = 5;
@@ -28,7 +28,7 @@ console.log('Gía trị trung bình là: ',giatriTb)
 /**
  * Bài 3: Quy đổi tiền
  * Input: USD = 23.500, vnd
- * Proccess: usdtovnd = vnd * USD
+ * Process: usdtovnd = vnd * USD
  * Output: số tiền sau khi quy đổi
  */
 
@@ -40,7 +40,7 @@ console.log('Số tiền sau khi quy đổi là: ', usdtovnd);
 /**
  * Bài 4: Tính diện tích, chu vi hình chữ nhật
  * Input: chiều dài, chiều rộng
- * Proccess: dientich = dai * rong, chuvi = (dai + rong ) * 2
+ * Process: dientich = dai * rong, chuvi = (dai + rong ) * 2
  * Output: dientich, chuvi
  */
 
@@ -54,7 +54,7 @@ console.log('Chu vi hình chữ nhật là: ', chuVi)
 /**
  * Bài 5: Tính tổng 2 ký số
  * Input: num
- * Procces: sohangDV= num % 10, sohangChuc = num / 10 
+ * Process: sohangDV= num % 10, sohangChuc = num / 10 
  * Output: Tổng 2 ký số
  */
 
